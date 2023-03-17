5c7f5a2bfd3448db97216a1da86bf85d347cebeb
Chukwunonso Jide <chukwunonsojide.e@gmail.com>
2023-03-17 12:19:49 -0700

    Script uses printf to output code lines

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Uses printf to print code lines
+*Return: Always 0
+*/
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art, \n");
+	return (0);
+}
