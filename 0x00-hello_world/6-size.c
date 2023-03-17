d67d7b5494b0ee7a4ae0815555edcbd42bd73dc2
Chukwunonso Jide <chukwunonsojide.e@gmail.com>
2023-03-17 15:33:46 -0700

    Task 06 and 08 with changes made

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,6 +3,7 @@
 *main - Print sizes of object types
 *Return: Always 0
 */
+
 int main(void)
 {
 	char a;
@@ -10,10 +11,11 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-	printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of float: %lu byte(s) \n", (unsigned long)sizeof(f));
+
+	printf("size of char: %lu byte(S)\n", (unsigned long)sizeof(a));
+	printf("size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
+	printf("size of long int: %lu byte(S)\n", (unsigned long)sizeof(c));
+	printf("size of long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
+	printf("size of float: %lu byte(S) \n", (unsigned long)sizeof(f));
 	return (0);
 }
