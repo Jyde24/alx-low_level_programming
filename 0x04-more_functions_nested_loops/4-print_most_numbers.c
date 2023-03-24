989de9c11d7eada8b36c543c9f064e1f48394910
Chukwunonso Jide <chukwunonsojide.e@gmail.com>
2023-03-24 08:20:02 -0700

    Task 4 - Corrections to print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print num except 2 and 4
+ * print_most_numbers - print numbers 01356789
  *
- * return: void
+ * Return: void
  */
 
 void print_most_numbers(void)
 {
-char c;
+	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	if (!(c == '2' || c == '4'))
-	_putchar(c);
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
 	_putchar('\n');
 }
