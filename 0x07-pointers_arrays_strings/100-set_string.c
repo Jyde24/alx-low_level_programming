a70a2198f59d85727cb82acc9e79517180448ce6
Chukwunonso Jide <chukwunonsojide.e@gmail.com>
2023-04-05 03:24:36 -0700

    Even More Pointers, Arrays and Strings

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * set_string - sets the value of a pointer to a char
+ * set_string - The function sets the value of a pointer to a char.
  * @s: pointer to pointer
- * @to: pointer char
+ * @to: The pointer char
  */
 
 void set_string(char **s, char *to)
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,26 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - Entry point
- * @a: input
- * @size: input
- * Return: Always 0 (Success)
+ * print_diagsums - Function prints sum of the two diagonals of a square matrix
+ * @a: Matrix
+ * @size: size of a
+ * Return: return void
  */
 void print_diagsums(int *a, int size)
 {
-	int sum1, sum2, y;
+	int x, sum1, sum2;
 
 	sum1 = 0;
 	sum2 = 0;
 
-	for (y = 0; y < size; y++)
+	for (x = 0; x < size; x++)
 	{
-		sum1 = sum1 + a[y * size + y];
+		sum1 += a[x];
+		a += size;
 	}
 
-	for (y = size - 1; y >= 0; y--)
+	for (x = size - 1; x >= 0; x--)
 	{
-		sum2 += a[y * size + (size - y - 1)];
+		sum2 += a[x * size + (size - x - 1)];
 	}
 
 	printf("%d, %d\n", sum1, sum2);
