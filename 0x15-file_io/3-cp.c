8d90f4fb21d1627aeec3888f74adc69472b09728
Chukwunonso Jide <chukwunonsojide.e@gmail.com>
2023-05-08 15:49:45 -0700

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,12 +11,12 @@ int main(int ac, char **av)
 	char buffer[BUFSIZ];
 
 	if (ac != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from"
-				"file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
+			exit(97);
 	feed = open(av[1], O_RDONLY);
 	if (feed == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from"
-				"file %s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+				av[1]), exit(98);
 	feed2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (feed2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]),
