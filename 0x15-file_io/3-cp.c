5805b2e273eb2f2f63c135bc83dcf413a440dd3c
Jyde24 <chukwunonsojide.e@gmail.com>
2023-07-10 15:33:45 -0700

    File input/output Tasks

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,77 +1,36 @@
 #include "main.h"
-#include <stdio.h>
-
-int close_error(int fd);
-void read_error(char *filename);
-void write_error(char *filename);
-
 /**
- * main - copies the content of a file to another file.
- * @argc: argument counter.
- * @argv: argument vector.
- * Return: always return 0, or exit.
+ * main - copies the content of a file to another file
+ * @ac: number of arguments
+ * @av: arguments
+ * Return: 1 on success , exit 97, 98, 99, 100 on error
  */
-int main(int argc, char *argv[])
+int main(int ac, char **av)
 {
-	char buffer[1024];
-	int fd_file_from, fd_file_to;
-	ssize_t bytes_counted = 1;
+	int closestatus, closestatus2, readcount, writecount, fd, fd2;
+	char buffer[BUFSIZ];
 
-	/* check number of argument */
-	if (argc != 3)
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	while ((readcount = read(fd, buffer, BUFSIZ)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		writecount = (write(fd2, buffer, readcount));
+		if (writecount != readcount)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
-	/* open and check fd for file_from */
-	fd_file_from = open(argv[1], O_RDONLY);
-	if (fd_file_from < 0)
-	{
-		read_error(argv[1]);
-		exit(98);
-	}
-	/* open and check fd for file_to */
-	fd_file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_file_to < 0)
-	{
-		write_error(argv[2]);
-		close_error(fd_file_from);
-		exit(99);
-	}
-
-	/* copy content - loop checks end of file (file_from) */
-	while (bytes_counted)
-	{
-		/* read the next 1020 bytes in file_from and check read error */
-		bytes_counted = read(fd_file_from, buffer, 1024);
-		if (bytes_counted < 0)
-		{
-			read_error(argv[1]);
-			close_error(fd_file_from);
-			close_error(fd_file_to);
-			exit(98);
-		}
-		/* check buffer end of file */
-		if (bytes_counted == 0)
-			break;
-		/* write the output in file_to and check write error */
-		bytes_counted = write(fd_file_to, buffer, bytes_counted);
-		if (bytes_counted < 0)
-		{
-			write_error(argv[2]);
-			close_error(fd_file_from);
-			close_error(fd_file_to);
-			exit(99);
-		}
-	}
-
-	/* close and check fd for file_from and file_to */
-	if (close_error(fd_file_from) < 0)
-	{
-		close_error(fd_file_to);
-		exit(100);
-	}
-	if (close_error(fd_file_to) < 0)
-		exit(100);
+	if (readcount == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	closestatus = close(fd);
+	if (closestatus == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+	closestatus2 = close(fd2);
+	if (closestatus2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
 	return (0);
 }
