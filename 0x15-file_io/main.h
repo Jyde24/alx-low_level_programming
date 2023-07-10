4238df9c4c9b8f10c2c951681ee65bafbf0427a9
Jyde24 <chukwunonsojide.e@gmail.com>
2023-07-10 15:59:01 -0700

    File input/output Tasks

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,29 +10,6 @@
 
 #define BUFFER_SIZE 1024
 
-/**
- * struct - structure for Elf header
- * ElfHeader - Structure representing the ELF header
- */
-typedef struct
-{
-        unsigned char e_ident[16];
-        uint16_t e_type;
-        uint16_t e_machine;
-        uint32_t e_version;
-        uint64_t e_entry;
-        uint64_t e_phoff;
-        uint64_t e_shoff;
-        uint32_t e_flags;
-        uint16_t e_ehsize;
-        uint16_t e_phentsize;
-        uint16_t e_phnum;
-        uint16_t e_shentsize;
-        uint16_t e_shnum;
-        uint16_t e_shstrndx;
-} ElfHeader;
-
-
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
